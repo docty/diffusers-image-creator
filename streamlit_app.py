5549560fa0b0c8cf0f40e6f7eeae3bb77040d8cc
Henry <sensork82@gmail.com>
2025-04-10 00:41:34 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-#from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
+from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
 import torch
 #from PIL import Image
 
@@ -7,19 +7,19 @@ import torch
 st.title("Image Generation:2")
 
  
-#@st.cache_resource
-# def load_pipeline():
-#     pipe = DiffusionPipeline.from_pretrained(
-#         "CompVis/stable-diffusion-v1-4",
-#         torch_dtype=torch.float16,
-#     )
-#     pipe.to("cuda" if torch.cuda.is_available() else "cpu")
+@st.cache_resource
+def load_pipeline():
+    pipe = DiffusionPipeline.from_pretrained(
+        "CompVis/stable-diffusion-v1-4",
+        torch_dtype=torch.float16,
+    )
+    pipe.to("cuda" if torch.cuda.is_available() else "cpu")
 
-#     pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
+    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
 
-#     pipe.enable_attention_slicing()
+    pipe.enable_attention_slicing()
   
-#     return pipe
+    return pipe
 
 #pipeline = load_pipeline()
 
