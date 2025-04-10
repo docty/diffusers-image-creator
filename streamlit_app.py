6488a9187e7ec2aadb47ff8f62acc0e7387d1788
Henry <sensork82@gmail.com>
2025-04-10 00:35:34 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
+#from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
 import torch
 from PIL import Image
 
@@ -7,25 +7,25 @@ from PIL import Image
 st.title("Image Generation:2")
 
  
-@st.cache_resource
-def load_pipeline():
-    pipe = DiffusionPipeline.from_pretrained(
-        "stable-diffusion-v1-5/stable-diffusion-v1-5",
-        torch_dtype=torch.float16,
-    )
-    pipe.to("cuda" if torch.cuda.is_available() else "cpu")
+#@st.cache_resource
+# def load_pipeline():
+#     pipe = DiffusionPipeline.from_pretrained(
+#         "CompVis/stable-diffusion-v1-4",
+#         torch_dtype=torch.float16,
+#     )
+#     pipe.to("cuda" if torch.cuda.is_available() else "cpu")
 
-    pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
+#     pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipeline.scheduler.config)
 
-    pipe.enable_attention_slicing()
+#     pipe.enable_attention_slicing()
   
-    return pipe
+#     return pipe
 
 #pipeline = load_pipeline()
 
 prompt = st.text_input("Enter a prompt to generate an image:", value="pipeline under the sea")
 
-if st.button("Generate Image"):
-    with st.spinner("Generating image..."):
-        image = pipe(prompt).images[0]
-        st.image(image, caption="Generated Image", use_column_width=True)
+# if st.button("Generate Image"):
+#     with st.spinner("Generating image..."):
+#         image = pipe(prompt).images[0]
+#         st.image(image, caption="Generated Image", use_column_width=True)
